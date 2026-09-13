d81c3e3d13787b88d3ce51d58882f37d5dbe11c5
REPULSor272 <amin.kurbanov.1997@mail.ru>
2026-09-13 17:27:53 +0300

    Добавил два новых файла .env для локальной разработки и для серверной версии и чуть поменял app-api.jsx

--- sdo-react-main/sdo/src/api/app-api.jsx
+++ sdo-react-main/sdo/src/api/app-api.jsx
@@ -1,14 +1,11 @@
 import axios from "axios";
 
-export const appApiIns = axios.create(
-  {
-        baseURL: 'http://localhost:8000/',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-    }
+export const appApiIns = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
   }
-)
+});
 
 appApiIns.interceptors.request.use(
   (config) => {

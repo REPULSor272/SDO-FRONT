cf54c46795c96508d87f823815c1e03162b1389c
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2026-09-18 18:43:02 +0300

    Добавлена страница

---
+++ sdo-react-main/sdo/src/pages/CheckLabTeacher/CheckLabTeacher.jsx
@@ -0,0 +1,177 @@
+import * as S from './CheckLabTeacher.styles'
+import { useParams } from 'react';
+import { getTaskById } from '../../api/file-api';
+import React, { useState, useEffect } from 'react';
+import { getGroups } from '../../api/teacher-api';
+import StudentLab from './components/StudentLab';
+
+
+const CheckLabTeacher = () => {
+    /*const { taskId } = useParams();
+    useEffect(() => {
+        console.log('Загрузка задачи с ID:', taskId);
+        getTaskById(taskId)
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((error) => {
+            console.error('Ошибка загрузки задачи:', error.message);
+          });
+      }, [taskId]);*/
+
+    const [searchValue, setSearchValue] = useState("");
+    const [groups, setGroups] = useState([]);
+    const [selectedGroup, setSelectedGroup] = useState('')
+    const [selectedSort, setSelectedSort] = useState('')
+
+    const handleSearchChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+  const handleSelectGroup = (e) => {
+    setSelectedGroup(e.target.value); 
+  };
+  const handleSelectSort = (e) => {
+    setSelectedSort(e.target.value); 
+  };
+
+  const handleSearch = () => {
+    console.log("Поиск запущен!");
+  };
+
+    const fetchGroups = async () => {
+        try{
+            const response = await getGroups()
+            setGroups(response.data)
+        }catch(error){
+            console.error("Ошибка при загрузке групп:", error.message);
+            setGroups([]);
+        }}
+        
+    useEffect(()=>{
+        fetchGroups()
+    }, [])
+
+    ///Люди добрые, когда узнаете ручки для этого добра, сделайте их пожалуйста, уберите этот срам.
+    const description = 'Даны три целых числа. Найдите наибольшее из них (программа должна вывести ровно одно целое число). Под наибольшим в этой задаче понимается число, которое не меньше, чем любое другое.'
+    const description_test_vovd = 'Вводится 3 числа'
+    const description_test_vovod = 'Выведете ответ на задачу'
+
+    const sorts = [
+            { id: '1', name: 'По фамилии (А-Я)' },
+            { id: '2', name: 'По убыванию оценки' },
+            { id: '3', name: 'По возрастанию оценки' },
+        ]
+
+    const studentsData = [
+  {
+    id: 1,
+    fullName: 'Иванов Иван Сергеевич',
+    group: '211-365',
+    score: 85,
+    isSubmitted: true,
+  },
+  {
+    id: 2,
+    fullName: 'Петров Алексей Владимирович',
+    group: '211-366',
+    score: 45,
+    isSubmitted: true,
+  },
+  {
+    id: 3,
+    fullName: 'Сидорова Анна Дмитриевна',
+    group: '211-365',
+    score: null,
+    isSubmitted: false,
+  },
+  {
+    id: 4,
+    fullName: 'Кузнецов Михаил Андреевич',
+    group: '211-366',
+    score: 100,
+    isSubmitted: true,
+  },
+  {
+    id: 5,
+    fullName: 'Смирнова Елена Игоревна',
+    group: '211-365',
+    score: null,
+    isSubmitted: false,
+  },
+];
+    return (
+        <S.container>
+            <S.Description>
+                <S.DescriptionText>
+                    {description}
+                </S.DescriptionText>
+                <S.DescriptionTest>
+                    <S.DescriptionTextBold>Формат вводных данных</S.DescriptionTextBold>
+                    <S.DescriptionText>{description_test_vovd}</S.DescriptionText>
+                    <S.DescriptionTextBold>Формат выводных данных</S.DescriptionTextBold>
+                    <S.DescriptionText>{description_test_vovod}</S.DescriptionText>
+                    <S.DescriptionTextBold>Пример ввода</S.DescriptionTextBold>
+                    <S.DescriptionText>{description_test_vovd}</S.DescriptionText>
+                    <S.DescriptionTextBold>Пример вывода</S.DescriptionTextBold>
+                    <S.DescriptionText>{description_test_vovod}</S.DescriptionText>
+                </S.DescriptionTest>
+            </S.Description>
+            <S.StudentList>
+                <S.StudentListH1>Список студентов:</S.StudentListH1>
+                <S.StudentListUp>
+                <S.SearchInputContainer>
+                    <S.SearchInput
+                    type="text"
+                    placeholder="Поиск студента"
+                    value={searchValue}
+                    onChange={handleSearchChange}
+                    />
+                    <S.SearchIcon onClick={() => handleSearch()} />
+                </S.SearchInputContainer>
+                <S.Sort>
+                <S.Select value={selectedSort} onChange={handleSelectSort}>
+                    <option value="">Сортировать по...</option>
+                    {(
+                    sorts.map((sort) => (
+                        <option key={sort.id} value={sort.name}>
+                        {sort.name}
+                        </option>
+                    ))
+                    )}
+                </S.Select>
+
+                <S.Select value={selectedGroup} onChange={handleSelectGroup}>
+                    <option value="">Группа</option>
+                    {groups.map((group) => (
+                    <option key={group.id} value={group.name}>
+                        {group.name}
+                    </option>
+                    ))}
+                </S.Select>
+                </S.Sort>
+                </S.StudentListUp>
+                {studentsData
+                .filter((student) => {if (searchValue === '') {return true} return student.fullName.toLowerCase().includes(searchValue.toLowerCase())})
+                .filter((student) => {if (selectedGroup === '') {return true} return student.group === selectedGroup})
+                .slice()
+                .sort((a,b) => {
+                    if (selectedSort === "По фамилии (А-Я)"){
+                        return a.fullName.localeCompare(b.fullName, 'ru');
+                    }
+                    if (selectedSort === "По убыванию оценки"){
+                        return b.score - a.score;
+                    }
+                    if (selectedSort === "По возрастанию оценки"){
+                        return a.score - b.score;
+                    }
+                })
+                .map((student) => {
+                    return <StudentLab key = {student.id} name = {student.fullName} isSubmitted = {student.isSubmitted} score = {student.score} ></StudentLab>
+                })}
+            </S.StudentList>
+        </S.container>
+    )
+}
+
+export default CheckLabTeacher
---
+++ sdo-react-main/sdo/src/pages/CheckLabTeacher/components/StudentLab.jsx
@@ -0,0 +1,83 @@
+import * as S from '../CheckLabTeacher.styles'
+import { ReactComponent as VectorIcon} from '../../../img/Vector.svg'
+import { useState } from 'react';
+
+const StudentLab = ({name, isSubmitted, score}) => {
+
+    const [checked, setChecked] = useState(false);
+    const [isEditingScore, setIsEditingScore] = useState(false)
+    const [scoreInput, setScoreInput] = useState('')
+
+    const DowlandFile = () => {
+        console.log('Как будет ручка, сделать запрос')
+    }
+
+    const changeEsp = () => {
+        setIsEditingScore((prev) => !prev)
+    }
+
+    const handleSaveScore = () => {
+        if (!isNaN(Number(scoreInput)) && Number(scoreInput) >= 0 && Number(scoreInput) <= 100) {
+            setScoreInput(scoreInput)
+        }
+    }
+
+    const scoreChange = () => {
+        if (scoreInput === ''){
+            return <>Изменить оценку</>
+        }
+        return <S.StudentLabChange>Оценка изменена <span style={{fontWeight: '600', marginLeft: '10px', color: scoreInput > 75 ? '#21B200' : '#FF7070' }}>{scoreInput}/100</span></S.StudentLabChange>
+    }
+
+    return (
+        <S.StudentsLab $color={isSubmitted ? (score > 75 ? "#E6F4CF" : "#F9DFDF8C") : "#F0F0F0"}>
+            <S.StudentsLabText>
+                {name}
+            </S.StudentsLabText>
+            {isSubmitted ? (
+            <S.StudentLabAction>
+                <S.StudentsLabtn>Просмотр результатов</S.StudentsLabtn>
+                <S.StudentLabResult $color={score > 75 ? "#21B200" : "#FF7070"}>{score}/100</S.StudentLabResult>
+                <S.StudentLabDowland onClick = {DowlandFile}><VectorIcon/></S.StudentLabDowland>
+                <S.StudentLabChange>
+                    {isEditingScore ? (
+                        <S.ScoreEditBox>
+                            Введите оценку
+                        <input
+                            type="text"
+                            value={scoreInput}
+                            onClick={(e) => e.stopPropagation()}
+                            onChange={(e) => setScoreInput(e.target.value)}
+                            onBlur={() => {
+                                handleSaveScore(); 
+                                setIsEditingScore(false);
+                            }}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                handleSaveScore();
+                                setIsEditingScore(false);
+                                }
+                            }}
+                            placeholder="0-100"
+                            autoFocus
+                        />
+                        </S.ScoreEditBox>
+                    ) : <span onClick={() => changeEsp()} style={{ cursor: 'pointer' }}>
+                            {scoreChange()}
+                        </span>}
+                </S.StudentLabChange>
+                <S.CheckboxContainer>
+                    <span>{!checked ? 'Сохранить:' : 'Сохранено'}</span>
+                    <S.StyledCheckbox $checked={checked} onClick={() => setChecked((prev) => !prev)}>
+                        <svg viewBox="0 0 24 24">
+                        <polyline points="20 6 9 17 4 12" />
+                        </svg>
+                    </S.StyledCheckbox>
+                    </S.CheckboxContainer>
+            </S.StudentLabAction>) : (<S.StudentLabNo><S.StudentLabChange>Не оценено</S.StudentLabChange></S.StudentLabNo>)
+            }
+        </S.StudentsLab>
+    )
+}
+
+export default StudentLab
